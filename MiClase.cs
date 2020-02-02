4680592be0b5f1357d0e091918d995a3f608f4a6
Guillermo <gonzaleztrevijano@gmail.com>
2020-02-02 21:37:08 +0100

    Control_de_Acceso

---
+++ MiClase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OtroNamespace
+{
+    class MiClase
+    {
+        //Puede ser visto por cualquier clase
+        public void MetodoPublico()
+        {
+            Console.WriteLine("Llamando al metodo publico");
+        }
+
+        //Puede ser visto desde esta clase o cualquier clase de MiClase
+        protected void MetodoProtected()
+        {
+            Console.WriteLine("Llamando al metodo protected");
+        }
+
+        //Puede ser visto porcualquier clase dentro del mismo assembly (el proyecto actual)
+        internal void MetodoInternal()
+        {
+            Console.WriteLine("Llamando al metodo internal");
+        }
+
+        //Puede ser visto solo por esta clase
+        private void MetodoPrivado()
+        {
+            Console.WriteLine("Llamando al metodo privado");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using OtroNamespace;    //Usando el namespace de "MiClase" y con esta referencia podemos usar las clases y metodos de dicho namespace
+
+namespace Proyecto3_Control_de_Acceso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MiClase obj = new MiClase();
+            obj.MetodoPublico();
+            obj.MetodoInternal();
+            // obv.MetodoProtected(); no deja llamarlo pro su nivel de proteccion
+            // obv.MetodoPrivado(); no deja llamarlo pro su nivel de proteccion
+        }
+    }
+}
+
